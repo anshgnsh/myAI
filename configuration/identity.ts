e00c7a96b4a0a3a299baa84c6444c4b1bfff593f
Anushka Ganesh <148222921+anshgnsh@users.noreply.github.com>
2025-03-10 12:56:46 -0400

    Changed AI's name to 'BERRY BOT', changed AI assistant description
    'COMPASS' -> 'BERRY BOT'

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `YETI's Virtual Outfitter`;
+export const OWNER_NAME: string = `Purple Bowl's AI assistant`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `COMPASS`;
+export const AI_NAME: string = `Berry Bot`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
