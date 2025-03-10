21858b1e264d48ff6ca5686fa902e57e5069a82a
Anushka Ganesh <148222921+anshgnsh@users.noreply.github.com>
2025-03-10 12:59:43 -0400

    Changed AI's name to 'BERRY BOT'

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Purple Bowl's AI assistant`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `Berry Bot`;
+export const AI_NAME: string = `BERRY BOT`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
