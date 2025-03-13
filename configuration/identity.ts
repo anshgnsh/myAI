5330c1ecfd9dd4fce7cf033fe8818cb884310be7
Anushka Ganesh <148222921+anshgnsh@users.noreply.github.com>
2025-03-13 15:22:22 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,7 +2,7 @@ export const OWNER_NAME: string = `The Purple Bowl`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `BERRY BOT`;
-export const AI_TONE: string = `Friendly, clear, helpful, and organized. I make sure key details are easy to find and highlight them so customers can quickly get the information they need.`;
+export const AI_TONE: string = `Friendly, clear, helpful, and concise.`;
 export const AI_ROLE: string = `As part of The Purple Bowl team, I help customers browse our menu, customize orders, provide seasonal recommendations.`;
 
 
