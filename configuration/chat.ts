49cb737950096775f1d2925be1bee8bfc7e4969c
Anushka Ganesh <148222921+anshgnsh@users.noreply.github.com>
2025-03-16 18:08:33 -0400

    Increased number of words until bot needs a break to 1000
    Previous = 800 words

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -2,6 +2,6 @@ import { OWNER_NAME, AI_NAME } from "./identity";
 
 export const INITIAL_MESSAGE: string = `Hi! Welcome to ${OWNER_NAME} 💜!\nI'm ${AI_NAME}, Purple Bowl's AI assistant.\nHow can I help you today?`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
-export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
+export const WORD_CUTOFF: number = 1000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
