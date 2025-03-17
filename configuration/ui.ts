c811fe161e1d62bec7aeb7c38b703b44086efb1e
Anushka Ganesh <148222921+anshgnsh@users.noreply.github.com>
2025-03-16 22:29:15 -0400

    Changed name to "acAI"

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `BERRY BOT`;
+export const CHAT_HEADER: string = `AçAI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Custom AI by Anushka Ganesh @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear chat history`;
-export const PAGE_TITLE: string = `BERRY BOT`;
+export const PAGE_TITLE: string = `AçAI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
