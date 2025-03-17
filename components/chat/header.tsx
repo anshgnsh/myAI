ddedffc33fcf0aaedb537dd15614ee784477dbe3
Anushka Ganesh <148222921+anshgnsh@users.noreply.github.com>
2025-03-17 12:34:09 -0400

    Updated header

--- components/chat/header.tsx
+++ components/chat/header.tsx
@@ -17,27 +17,31 @@ export default function ChatHeader({
   clearMessages: () => void;
 }) {
   return (
-    <div className="z-10 flex justify-center items-center fixed top-0 w-full p-5 bg-[#eeece7]">
-      <div className="flex w-full">
-        <div className="flex-0 w-[100px]"></div>
-        <div className="flex-1 flex justify-center items-center gap-2">
-          <AILogo />
-          <p>{CHAT_HEADER}</p>
-        </div>
-        <div className="flex-0 w-[100px] flex justify-end items-center">
-          <Button
-            onClick={clearMessages}
-            className="gap-2 shadow-sm bg-[#7ec7b7] text-white border border-white hover:bg-[#44296d] hover:text-white hover:border-[#44296d]" // changed
-            variant="outline"
-            size="sm"
-          >
-            <EraserIcon className="w-4 h-4" />
-            <span>{CLEAR_BUTTON_TEXT}</span>
-          </Button>
+    <div>
+      {/* Header */}
+      <div className="z-10 flex justify-center items-center fixed top-0 w-full p-5 bg-[#eeece7] shadow-[0_10px_15px_-3px_rgba(255,255,255,1)]">
+        <div className="flex w-full">
+          <div className="flex-0 w-[100px]"></div>
+          <div className="flex-1 flex justify-center items-center gap-2">
+            <AILogo />
+            <p>{CHAT_HEADER}</p>
+          </div>
+          <div className="flex-0 w-[100px] flex justify-end items-center">
+            <Button
+              onClick={clearMessages}
+              className="gap-2 shadow-sm bg-[#7ec7b7] text-white border border-white hover:bg-[#44296d] hover:text-white hover:border-[#44296d]"
+              variant="outline"
+              size="sm"
+            >
+              <EraserIcon className="w-4 h-4" />
+              <span>{CLEAR_BUTTON_TEXT}</span>
+            </Button>
+          </div>
         </div>
       </div>
+      
       {/* Black Line Separator */}
-      <div className="w-full h-[2px] bg-black mt-2"></div>
+      <div className="w-full h-[2px] bg-black mt-20"></div>
     </div>
   );
 }
