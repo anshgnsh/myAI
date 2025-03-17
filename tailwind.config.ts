cb34185189a1ca333deb9a2e55f98086ee375dad
Anushka Ganesh <148222921+anshgnsh@users.noreply.github.com>
2025-03-16 23:39:04 -0400

    Changed background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
-  			background: 'hsl(var(--background))',
+  			background: '#eeece7', // Updated background color
   			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
