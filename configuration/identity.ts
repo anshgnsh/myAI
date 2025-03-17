bd88899db100383fe8facd9f5443e35a53967116
Anushka Ganesh <148222921+anshgnsh@users.noreply.github.com>
2025-03-16 21:53:26 -0400

    Updated owner description

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `The Purple Bowl`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `THE PURPLE BOWL IS A LOCAL EATERY AND HANGOUT IN DOWNTOWN CHAPEL HILL SERVING AÇAÍ BOWLS, ARTISAN TOAST, ESPRESSO DRINKS, AND HEALTHY EATS. `;
 
 export const AI_NAME: string = `AçAI`;
 export const AI_TONE: string = `Friendly, clear, concise, and helpful.`;
